53e47d37ab242149e69b09f4cc02650495d22cee
Virendra <virendra.thakur31@gmail.com>
2023-07-17 21:27:50 +0530

    IPageNavigationService and PageNavigationService added.

---
+++ Services/Implementations/PageNavigationService.cs
@@ -0,0 +1,13 @@
+﻿using MAUISampleApp.MVVM.Services.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MAUISampleApp.MVVM.Services.Implementations
+{
+    public class PageNavigationService : IPageNavigationService
+    {
+    }
+}

6620b491893c7ca83eb8af75fb78d57a8e593b5f
Virendra <virendra.thakur31@gmail.com>
2023-07-18 22:23:36 +0530

    NavigateTo added to PageNavigationService

--- Services/Implementations/PageNavigationService.cs
+++ Services/Implementations/PageNavigationService.cs
@@ -1,6 +1,7 @@
 ﻿using MAUISampleApp.MVVM.Services.Interfaces;
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,5 +10,49 @@ namespace MAUISampleApp.MVVM.Services.Implementations
 {
     public class PageNavigationService : IPageNavigationService
     {
+        public string CurrentPageKey { get; set; }
+
+        protected INavigation Navigation
+        {
+            get
+            {
+                INavigation? navigation = Application.Current?.MainPage?.Navigation;
+                if (navigation is not null)
+                    return navigation;
+
+                else return null;
+
+            }
+        }
+
+        public void GoBack()
+        {
+            throw new NotImplementedException();
+        }
+
+        public async void NavigateTo(Page pageKey)
+        {
+            await Navigation.PushAsync(pageKey);
+        }
+
+        public void NavigateTo(Page pageKey, object parameter)
+        {
+            try
+            {
+
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine(ex.ToString());
+                Debugger.Break();
+            }
+        }
+
+        public void PopToRoot()
+        {
+            throw new NotImplementedException();
+        }
     }
+
+    public interface IModalPage { }
 }

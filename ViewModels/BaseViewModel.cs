f55623456737098fd7308708d178bf7e659f307e
Virendra <virendra.thakur31@gmail.com>
2023-07-19 21:44:25 +0530

    BaseViewModel updated.

--- ViewModels/BaseViewModel.cs
+++ ViewModels/BaseViewModel.cs
@@ -1,12 +1,15 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
 
 namespace MAUISampleApp.MVVM.ViewModels
 {
-    public class BaseViewModel
+    public class BaseViewModel : INotifyPropertyChanged
     {
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        public void OnPropertyChanged([CallerMemberName] string name = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+        }
     }
 }

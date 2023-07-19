6a6c8dc86cd2a63bf2bfe9d734f206e4ac0777ef
Virendra <virendra.thakur31@gmail.com>
2023-07-19 21:03:00 +0530

    DashboardItem added

---
+++ Models/DashboardItem.cs
@@ -0,0 +1,10 @@
+﻿namespace MAUISampleApp.MVVM.Models
+{
+    public class DashboardItem
+    {
+        public int Id { get; set; }
+        public string ItemName { get; set; }
+        public string Itemicon { get; set; }
+        public Page page { get; set; }
+    }
+}

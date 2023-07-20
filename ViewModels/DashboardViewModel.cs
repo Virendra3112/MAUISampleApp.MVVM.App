34777e473a5a2005c5512f0a291d909cfd98450d
Virendra <virendra.thakur31@gmail.com>
2023-07-20 20:15:24 +0530

    DashboardViewModel updated.

--- ViewModels/DashboardViewModel.cs
+++ ViewModels/DashboardViewModel.cs
@@ -5,6 +5,7 @@ using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
 
 namespace MAUISampleApp.MVVM.ViewModels
 {
@@ -17,16 +18,45 @@ namespace MAUISampleApp.MVVM.ViewModels
             set { _categoryList = value; OnPropertyChanged(); }
         }
 
+        public ICommand MenuItemCommand { get; set; }
+
         public DashboardViewModel()
         {
+            MenuItemCommand = new Command(MenuSelected);
+
+
             CategoryList = new ObservableCollection<DashboardItem>();
             CategoryList.Add(new DashboardItem { ItemName = "Custom UI", Itemicon = "icon.png" });
             CategoryList.Add(new DashboardItem { ItemName = "1 Item", Itemicon = "icon.png" });
             CategoryList.Add(new DashboardItem { ItemName = "2 Item", Itemicon = "icon.png" });
             CategoryList.Add(new DashboardItem { ItemName = "3 Item", Itemicon = "icon.png" });
-           
-           
-
         }
+
+
+        private async void MenuSelected(object obj)
+        {
+            try
+            {
+                if (obj != null)
+                {
+                    var model = obj as DashboardItem;
+
+                }
+            }
+            catch (Exception ex) { }
+        }
+
+        private async void NavigateTo(Page page, string pageName)
+        {
+            try
+            {
+                //await Navigation.PushAsync(page);
+            }
+            catch (Exception ex)
+            {
+
+            }
+        }
+
     }
 }

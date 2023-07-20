86f7b1b11904fcf4a70674baa4357600d9f2e968
Virendra <virendra.thakur31@gmail.com>
2023-07-20 19:58:26 +0530

    DashboardViewModel updated.

--- ViewModels/DashboardViewModel.cs
+++ ViewModels/DashboardViewModel.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using MAUISampleApp.MVVM.Models;
+using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,5 +10,23 @@ namespace MAUISampleApp.MVVM.ViewModels
 {
     public class DashboardViewModel : BaseViewModel
     {
+        private ObservableCollection<DashboardItem> _categoryList;
+        public ObservableCollection<DashboardItem> CategoryList
+        {
+            get { return _categoryList; }
+            set { _categoryList = value; OnPropertyChanged(); }
+        }
+
+        public DashboardViewModel()
+        {
+            CategoryList = new ObservableCollection<DashboardItem>();
+            CategoryList.Add(new DashboardItem { ItemName = "Custom UI", Itemicon = "icon.png" });
+            CategoryList.Add(new DashboardItem { ItemName = "1 Item", Itemicon = "icon.png" });
+            CategoryList.Add(new DashboardItem { ItemName = "2 Item", Itemicon = "icon.png" });
+            CategoryList.Add(new DashboardItem { ItemName = "3 Item", Itemicon = "icon.png" });
+           
+           
+
+        }
     }
 }

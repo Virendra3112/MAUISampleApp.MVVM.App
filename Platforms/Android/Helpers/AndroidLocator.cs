c5a761ffaed97a04959797779a36d0d35536308e
Virendra <virendra.thakur31@gmail.com>
2023-07-21 21:46:17 +0530

    AndroidLocator added.

---
+++ Platforms/Android/Helpers/AndroidLocator.cs
@@ -0,0 +1,14 @@
+﻿using Autofac;
+using MAUISampleApp.MVVM.Helpers;
+
+namespace MAUISampleApp.MVVM.Platforms.Android.Helpers
+{
+    public class AndroidLocator : AppLocator
+    {
+        public static void Initialize()
+        {
+            var builder = new ContainerBuilder();
+            Init(builder);
+        }
+    }
+}

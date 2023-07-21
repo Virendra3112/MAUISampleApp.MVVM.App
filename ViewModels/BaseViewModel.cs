9a3b14f0bf8f816c4bc6ebb80c7386b21c9e5b6c
Virendra <virendra.thakur31@gmail.com>
2023-07-21 21:43:43 +0530

    commit

--- ViewModels/BaseViewModel.cs
+++ ViewModels/BaseViewModel.cs
@@ -1,15 +1,27 @@
-﻿using System.ComponentModel;
+﻿using CommonServiceLocator;
+using MAUISampleApp.MVVM.Services.Interfaces;
+using System.ComponentModel;
 using System.Runtime.CompilerServices;
 
 namespace MAUISampleApp.MVVM.ViewModels
 {
     public class BaseViewModel : INotifyPropertyChanged
     {
-
+        #region propertychanged
         public event PropertyChangedEventHandler PropertyChanged;
         public void OnPropertyChanged([CallerMemberName] string name = "")
         {
             PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
         }
+
+        #endregion
+
+        protected readonly IPageNavigationService PageNavigationService;
+
+        public BaseViewModel()
+        {
+            //PageNavigationService = ServiceLocator.Current.GetInstance<IPageNavigationService>();
+        }
+
     }
 }

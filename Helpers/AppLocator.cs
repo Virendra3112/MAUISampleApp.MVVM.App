b835f1afe338c33e5730b72175c7f02875f26d49
Virendra <virendra.thakur31@gmail.com>
2023-07-21 21:39:27 +0530

    AppLocator added.

---
+++ Helpers/AppLocator.cs
@@ -0,0 +1,26 @@
+﻿using Autofac;
+
+namespace MAUISampleApp.MVVM.Helpers
+{
+    public class AppLocator
+    {
+        public static Autofac.IContainer Container { get; set; }
+
+
+        protected static void Init(ContainerBuilder builder)
+        {
+            RegisterServices(builder);
+            RegisterViewModel(builder);
+        }
+
+        private static void RegisterViewModel(ContainerBuilder builder)
+        {
+            throw new NotImplementedException();
+        }
+
+        private static void RegisterServices(ContainerBuilder builder)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}

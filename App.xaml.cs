01f59e5065863355321e557bb94b58de8596894b
Virendra <virendra.thakur31@gmail.com>
2023-07-22 23:42:48 +0530

    commit

--- App.xaml.cs
+++ App.xaml.cs
@@ -1,11 +1,19 @@
-﻿namespace MAUISampleApp.MVVM;
+﻿using Autofac.Extras.CommonServiceLocator;
+using CommonServiceLocator;
+using MAUISampleApp.MVVM.Helpers;
+
+namespace MAUISampleApp.MVVM;
 
 public partial class App : Application
 {
-	public App()
-	{
-		InitializeComponent();
+    public App()
+    {
+        InitializeComponent();
 
-		MainPage = new AppShell();
-	}
+
+        if (!ServiceLocator.IsLocationProviderSet)
+            ServiceLocator.SetLocatorProvider(() => new AutofacServiceLocator(AppLocator.Container));
+
+        MainPage = new AppShell();
+    }
 }

db828dc88727cba20f06258cd682f02c22adf51d
Virendra <virendra.thakur31@gmail.com>
2023-07-23 21:39:16 +0530

    AppBootStrapper, AndroidBootStrapper added.

---
+++ Helpers/AppBootStrapper.cs
@@ -0,0 +1,27 @@
+﻿using Autofac;
+using MAUISampleApp.MVVM.ViewModels;
+
+namespace MAUISampleApp.MVVM.Helpers
+{
+    public class AppBootStrapper : AppLocator
+    {
+        protected static void Init(ContainerBuilder builder)
+        {
+            RegisterCommon(builder);
+            RegisterAppServices(builder);
+            RegisterAppViewModel(builder);
+        }
+
+        /// <summary>
+        /// Registers the services.
+        /// </summary>
+        private static void RegisterAppServices(ContainerBuilder containerBuilder)
+        {
+        }
+
+        private static void RegisterAppViewModel(ContainerBuilder builder)
+        {
+            builder.RegisterAssemblyTypes(typeof(BaseViewModel).Assembly).InstancePerLifetimeScope().AsImplementedInterfaces().AsSelf().AssignableTo<BaseViewModel>();
+        }
+    }
+}

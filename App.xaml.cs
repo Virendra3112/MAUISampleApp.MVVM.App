99b576c0ac9bedc5366d5103d6a2ee9bbebd20fc
Virendra <virendra.thakur31@gmail.com>
2023-07-23 21:58:48 +0530

    commit.

--- App.xaml.cs
+++ App.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using Autofac.Extras.CommonServiceLocator;
 using CommonServiceLocator;
 using MAUISampleApp.MVVM.Helpers;
+using Microsoft.Maui.Devices.Sensors;
 
 namespace MAUISampleApp.MVVM;
 
@@ -11,9 +12,31 @@ public partial class App : Application
         InitializeComponent();
 
 
-        if (!ServiceLocator.IsLocationProviderSet)
-            ServiceLocator.SetLocatorProvider(() => new AutofacServiceLocator(AppLocator.Container));
+
+        //if (!ServiceLocator.IsLocationProviderSet)
+        //    ServiceLocator.SetLocatorProvider(() => new AutofacServiceLocator(AppLocator.Container));
+
+        Locator();
 
         MainPage = new AppShell();
+
+    }
+
+    private async Task Locator()
+    {
+        try
+        {
+
+            await Task.Delay(5000);
+
+            if (!ServiceLocator.IsLocationProviderSet)
+                ServiceLocator.SetLocatorProvider(() => new AutofacServiceLocator(AppLocator.Container));
+
+        }
+        catch (Exception ex)
+        {
+
+        }
+
     }
 }

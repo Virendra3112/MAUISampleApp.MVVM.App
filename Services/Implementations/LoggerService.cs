bed5e866e1671da328b349edfb32ff231e4b16cf
Virendra <virendra.thakur31@gmail.com>
2023-07-31 23:17:57 +0530

    LoggerService added.

---
+++ Services/Implementations/LoggerService.cs
@@ -0,0 +1,26 @@
+﻿using MAUISampleApp.MVVM.Services.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MAUISampleApp.MVVM.Services.Implementations
+{
+    public class LoggerService : ILoggerService
+    {
+        public LoggerService()
+        {
+        }
+
+        public void LogDetails(string className, string methodName, string message, Exception ex)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void LogError(string className, string methodName, Exception ex)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}

03da219e18a9b2d7693d2133bb453a14e5f1a5c4
Virendra <virendra.thakur31@gmail.com>
2023-07-31 23:16:36 +0530

    ILoggerService added.

---
+++ Services/Interfaces/ILoggerService.cs
@@ -0,0 +1,8 @@
+﻿namespace MAUISampleApp.MVVM.Services.Interfaces
+{
+    public interface ILoggerService
+    {
+        void LogError(string className, string methodName, Exception ex);
+        void LogDetails(string className, string methodName, string message, Exception ex);
+    }
+}

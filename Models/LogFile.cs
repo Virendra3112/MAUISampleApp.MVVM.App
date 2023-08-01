7fb59a995aa39b751ef3dc260a713864825e61fd
Virendra <virendra.thakur31@gmail.com>
2023-08-01 23:03:47 +0530

    LogFile added.

---
+++ Models/LogFile.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MAUISampleApp.MVVM.Models
+{
+    public class LogFile
+    {
+        public string Class { get; set; }
+        public string MethodName { get; set; }
+        public string Log { get; set; }
+        public string Message { get; set; }
+        public Exception Exception { get; set; }
+    }
+}

fce72f3eccb7d15da6508c837e276a92171570ac
Virendra <virendra.thakur31@gmail.com>
2023-08-02 22:44:03 +0530

    LoggerService updated.

--- Services/Implementations/LoggerService.cs
+++ Services/Implementations/LoggerService.cs
@@ -1,4 +1,6 @@
-﻿using MAUISampleApp.MVVM.Services.Interfaces;
+﻿using MAUISampleApp.MVVM.Models;
+using MAUISampleApp.MVVM.Services.Interfaces;
+using MetroLog;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,9 +11,44 @@ namespace MAUISampleApp.MVVM.Services.Implementations
 {
     public class LoggerService : ILoggerService
     {
+        private static readonly ILogger Logitem = LoggerFactory.GetLogger(nameof(LoggerService));
+
         public LoggerService()
         {
         }
+
+        public Task<LogFile> DowloadLogsAsync()
+        {
+            try
+            {
+                Logitem.Info("DowloadLogsAsync");
+
+                //todo: download file
+
+                throw new NotImplementedException();
+            }
+            catch (Exception ex)
+            {
+                Logitem.Error("Oops something bad happened", ex);
+                throw ex;
+            }
+        }
+
+        //public async Task<LogFile> DowloadLogsAsync()
+        //{
+        //    Log.Info("DowloadAsync()");
+
+        //    try
+        //    {
+        //        //await _client.GetAsync();
+
+        //        return null;//ToDo
+        //    }
+        //    catch (Exception exception)
+        //    {
+        //        Log.Error("Oops something bad happened", exception);
+        //    }
+        //}
 
         public void LogDetails(string className, string methodName, string message, Exception ex)
         {

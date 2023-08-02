e1867af10eb0947efeba0f1d569a0a8643b9f233
Virendra <virendra.thakur31@gmail.com>
2023-08-02 22:43:52 +0530

    ILoggerService updated.

--- Services/Interfaces/ILoggerService.cs
+++ Services/Interfaces/ILoggerService.cs
@@ -1,8 +1,13 @@
-﻿namespace MAUISampleApp.MVVM.Services.Interfaces
+﻿using MAUISampleApp.MVVM.Models;
+
+namespace MAUISampleApp.MVVM.Services.Interfaces
 {
     public interface ILoggerService
     {
         void LogError(string className, string methodName, Exception ex);
         void LogDetails(string className, string methodName, string message, Exception ex);
+
+        Task<LogFile> DowloadLogsAsync();
     }
+
 }

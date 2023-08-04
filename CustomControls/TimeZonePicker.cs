e08c7e9643d9c60e738b62360ebb8b4ff994520b
Virendra <virendra.thakur31@gmail.com>
2023-08-04 21:30:22 +0530

    commit.

--- CustomControls/TimeZonePicker.cs
+++ CustomControls/TimeZonePicker.cs
@@ -1,11 +1,4 @@
-﻿using Microsoft.Maui.Controls;
-using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Microsoft.Maui.Controls.Shapes;
+﻿using Microsoft.Maui.Controls.Shapes;
 
 
 namespace MAUISampleApp.MVVM.CustomControls

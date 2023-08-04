aa77299d742b5b7860059f562c35a5a3d3fe8c88
Virendra <virendra.thakur31@gmail.com>
2023-08-04 21:43:55 +0530

    Extensions added.

---
+++ CustomControls/Extensions.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MAUISampleApp.MVVM.CustomControls
+{
+    public static class Extensions
+    {
+        private static readonly string[] UtcAliases = {
+        "UTC",
+        "Etc/UTC",
+        "Etc/UCT",
+        "Etc/Universal",
+        "Etc/Zulu",
+        "UCT",
+        "Universal",
+        "Zulu"
+    };
+
+        public static bool TimeZoneIsUtc(this string timeZoneId) =>
+            UtcAliases.Contains(timeZoneId, StringComparer.OrdinalIgnoreCase);
+
+        public static string FormatAsOffset(this TimeSpan offset) =>
+            (offset < TimeSpan.Zero ? "-" : "+") + offset.ToString(@"hh\:mm");
+
+        public static string AdjustTimeZoneDisplayText(this string s)
+        {
+            // Older globalization data may not have made this important change yet.
+            return s.Replace("Kiev", "Kyiv", StringComparison.CurrentCultureIgnoreCase);
+        }
+    }
+}

2ae5025f406bc9fa431d329b06e2032782edae38
Virendra <virendra.thakur31@gmail.com>
2023-08-04 21:43:33 +0530

    ITimeZoneResourceProvider added.

---
+++ CustomControls/ITimeZoneResourceProvider.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MAUISampleApp.MVVM.CustomControls
+{
+    public interface ITimeZoneResourceProvider
+    {
+        IReadOnlyList<TimeZoneResource> GetTimeZoneResources();
+        IReadOnlyList<string> GetTimeZoneIds();
+        string GetGenericName(string timeZoneId);
+        string? GetLocation(string timeZoneId);
+    }
+}

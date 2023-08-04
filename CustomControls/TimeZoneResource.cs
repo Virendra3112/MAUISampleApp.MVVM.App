ce44bf6a376b7c2fe304b77e3eeb8074afac281f
Virendra <virendra.thakur31@gmail.com>
2023-08-04 21:44:14 +0530

    TimeZoneResou added.rce

---
+++ CustomControls/TimeZoneResource.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MAUISampleApp.MVVM.CustomControls
+{
+    public class TimeZoneResource
+    {
+        public string Id { get; init; } = null!;
+        public string Name { get; init; } = null!;
+        public string? Location { get; init; }
+        public TimeZoneInfo TimeZone { get; init; } = null!;
+
+        public string BaseOffset => Id.TimeZoneIsUtc()
+            ? "UTC"
+            : $"UTC{TimeZone.BaseUtcOffset.FormatAsOffset()}";
+
+        public string CurrentOffset => Id.TimeZoneIsUtc()
+            ? "UTC"
+            : $"UTC{TimeZone.GetUtcOffset(DateTime.UtcNow).FormatAsOffset()}";
+
+        public override string ToString() => $"{Name} : ({CurrentOffset}) {Location}".TrimEnd();
+    }
+}

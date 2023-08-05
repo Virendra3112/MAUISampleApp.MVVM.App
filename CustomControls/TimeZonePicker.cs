f195a484e921569b1326c5d787473580c5ea3a43
Virendra <virendra.thakur31@gmail.com>
2023-08-05 19:58:05 +0530

    commit.

--- CustomControls/TimeZonePicker.cs
+++ CustomControls/TimeZonePicker.cs
@@ -1,14 +1,25 @@
-﻿using Microsoft.Maui.Controls.Shapes;
+﻿using System.Collections;
+using Microsoft.Maui.Controls.Shapes;
+
 
 
 namespace MAUISampleApp.MVVM.CustomControls
 {
-    public class TimeZonePicker : VerticalStackLayout
+    public class TimeZonePicker : VerticalStackLayout, IDisposable
     {
+        private readonly ITimeZoneResourceProvider _resourceProvider = new TimeZoneResourceProvider();
+
         public TimeZonePicker()
         {
+            CollectionView = GetCollectionView(_resourceProvider.GetTimeZoneResources());
             AddContent();
         }
+
+        public CollectionView CollectionView { get; }
+
+        public TimeZoneResource? SelectedItem => CollectionView.SelectedItem as TimeZoneResource;
+
+        public event EventHandler<SelectedItemChangedEventArgs>? SelectedItemChanged;
 
         private void AddContent()
         {
@@ -22,23 +33,99 @@ namespace MAUISampleApp.MVVM.CustomControls
             {
                 StrokeShape = new Rectangle(),
                 Padding = 2,
-                Content = GetCollectionView()
+                Content = CollectionView
             });
         }
 
-        private CollectionView GetCollectionView()
+        private CollectionView GetCollectionView(IEnumerable itemsSource)
         {
             var view = new CollectionView
             {
-                //ItemsSource = TODO,
-                //ItemTemplate = TODO,
+                ItemsSource = itemsSource,
+                ItemTemplate = GetItemTemplate(),
                 MaximumHeightRequest = 200,
                 SelectionMode = SelectionMode.Single
             };
 
             ((LinearItemsLayout)view.ItemsLayout).ItemSpacing = 10;
 
+            view.SelectionChanged += (_, args) =>
+            {
+                var previousSelection = args.PreviousSelection.FirstOrDefault() as TimeZoneResource;
+                var currentSelection = args.CurrentSelection.FirstOrDefault() as TimeZoneResource;
+                var eventArgs = new SelectedItemChangedEventArgs(previousSelection, currentSelection);
+                SelectedItemChanged?.Invoke(this, eventArgs);
+            };
+
             return view;
+        }
+
+        private static DataTemplate GetItemTemplate() =>
+            new(() =>
+            {
+                var layout = new VerticalStackLayout();
+
+                var name = new Label
+                {
+                    FontSize = 16,
+                    FontAttributes = FontAttributes.Bold
+                };
+                name.SetBinding(Label.TextProperty, nameof(TimeZoneResource.Name));
+                layout.Add(name);
+
+                var detail = new HorizontalStackLayout
+                {
+                    Spacing = 4
+                };
+
+                var offset = new Label
+                {
+                    FontSize = 12,
+                    FontAttributes = FontAttributes.Bold
+                };
+                offset.SetBinding(Label.TextProperty,
+                    new Binding(nameof(TimeZoneResource.CurrentOffset), stringFormat: "({0})"));
+                detail.Add(offset);
+
+                var location = new Label
+                {
+                    FontSize = 12,
+                    FontAttributes = FontAttributes.Italic
+                };
+                location.SetBinding(Label.TextProperty, nameof(TimeZoneResource.Location));
+                detail.Add(location);
+
+                layout.Add(detail);
+
+                return layout;
+            });
+
+        private void DisposeResources()
+        {
+            (_resourceProvider as IDisposable)?.Dispose();
+        }
+
+        public void Dispose()
+        {
+            DisposeResources();
+            GC.SuppressFinalize(this);
+        }
+
+        ~TimeZonePicker()
+        {
+            DisposeResources();
+        }
+
+        public class SelectedItemChangedEventArgs : EventArgs
+        {
+            public SelectedItemChangedEventArgs(TimeZoneResource? previousSelection, TimeZoneResource? currentSelection)
+            {
+                PreviousSelection = previousSelection;
+                CurrentSelection = currentSelection;
+            }
+
+            public TimeZoneResource? PreviousSelection { get; }
+            public TimeZoneResource? CurrentSelection { get; }
         }
     }
 }
--- CustomControls/TimeZoneResource.cs
+++ CustomControls/TimeZoneResource.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace MAUISampleApp.MVVM.CustomControls
+﻿namespace MAUISampleApp.MVVM.CustomControls
 {
     public class TimeZoneResource
     {

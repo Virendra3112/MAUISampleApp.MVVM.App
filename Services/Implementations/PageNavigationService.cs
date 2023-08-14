2130f42a0e3cff3e02f13eb9846259aa6a60d35b
Virendra <virendra.thakur31@gmail.com>
2023-08-14 22:11:35 +0530

    PageNavigationService updated.

--- Services/Implementations/PageNavigationService.cs
+++ Services/Implementations/PageNavigationService.cs
@@ -64,5 +64,15 @@ namespace MAUISampleApp.MVVM.Services.Implementations
                 }
             }
         }
+
+        public Task InitializeAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task NavigateToAsync(string route, IDictionary<string, object> routeParameters = null)
+        {
+            throw new NotImplementedException();
+        }
     }
 }

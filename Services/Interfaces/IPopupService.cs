c5dd362fafd14e54f6ccdfd5afd28530566bdeb6
Virendra <virendra.thakur31@gmail.com>
2023-08-15 23:17:50 +0530

    IPopupService added.

---
+++ Services/Interfaces/IPopupService.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MAUISampleApp.MVVM.Services.Interfaces
+{
+    public interface IPopupService
+    {
+        void DisplayAlert(string title, string message);
+        void DisplayAlert(string title, string message, string okButtonText, string canelButtonText);
+
+    }
+}

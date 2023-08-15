cb5b3487a3eb1dae60adb34bc7664aa2e82c1b91
Virendra <virendra.thakur31@gmail.com>
2023-08-15 23:19:49 +0530

    PopupService added.

---
+++ Services/Implementations/PopupService.cs
@@ -0,0 +1,27 @@
+﻿using MAUISampleApp.MVVM.Services.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MAUISampleApp.MVVM.Services.Implementations
+{
+    public class PopupService : IPopupService
+    {
+        public void DisplayAlert(string title, string message)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void DisplayAlert(string title, string message, string okButtonText, string canelButtonText)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Init()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}

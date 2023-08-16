c21e94d081b640cd8acb8a6ae17dc9a54e50339c
Virendra <virendra.thakur31@gmail.com>
2023-08-16 22:13:12 +0530

    IHttpService added.

---
+++ Services/Interfaces/IHttpService.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MAUISampleApp.MVVM.Services.Interfaces
+{
+    public interface IHttpService
+    {
+        Task<HttpResponseMessage> GetAsync(string url);
+
+        Task<HttpResponseMessage> PostAsync(string url, object data);
+
+        `
+    }
+}

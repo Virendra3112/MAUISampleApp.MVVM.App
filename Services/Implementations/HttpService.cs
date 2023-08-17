acc61d9042d23b3932fe4f1caaefe68e0749245c
Virendra <virendra.thakur31@gmail.com>
2023-08-17 21:43:11 +0530

    commit.

--- Services/Implementations/HttpService.cs
+++ Services/Implementations/HttpService.cs
@@ -1,12 +1,33 @@
 ﻿using MAUISampleApp.MVVM.Services.Interfaces;
+using System.Net;
 
 namespace MAUISampleApp.MVVM.Services.Implementations
 {
     public class HttpService : IHttpService
     {
+        HttpClient _httpClient;
+        HttpService()
+        {
+            _httpClient = new HttpClient();
+        }
         public Task<HttpResponseMessage> GetAsync(string url)
         {
-            throw new NotImplementedException();
+            try
+            {
+                var response = _httpClient.GetAsync(url);
+
+                if (response == null)
+                {
+                    return null;
+                    //HttpStatusCode.BadRequest;
+                }
+                return response;
+
+            }
+            catch (Exception ex)
+            {
+                return null;
+            }
         }
 
         public Task<HttpResponseMessage> PostAsync(string url, object data)

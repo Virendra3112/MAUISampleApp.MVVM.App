e43236a6843acc4ebad90ee7810875308bf0e89f
Virendra <virendra.thakur31@gmail.com>
2023-08-17 21:49:41 +0530

    AppVault added.

---
+++ Helpers/AppVault.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MAUISampleApp.MVVM.Helpers
+{
+    public class AppVault
+    {
+        //Base url
+        public string BaseUrl = "https://sampleBaseUrl.api.net";
+
+        //Get students data
+        public string GetStudents = "Student/GetStudents";
+    }
+}

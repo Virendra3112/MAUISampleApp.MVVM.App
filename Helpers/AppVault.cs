f5b572fbf9bf54eaa56cc9a300e27f6bf0dbe10b
Virendra <virendra.thakur31@gmail.com>
2023-08-18 21:51:23 +0530

    AppVault updated.

--- Helpers/AppVault.cs
+++ Helpers/AppVault.cs
@@ -1,17 +1,11 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace MAUISampleApp.MVVM.Helpers
+﻿namespace MAUISampleApp.MVVM.Helpers
 {
-    public class AppVault
+    public static class AppVault
     {
         //Base url
-        public string BaseUrl = "https://sampleBaseUrl.api.net";
+        public static string BaseUrl = "https://sampleBaseUrl.api.net";
 
         //Get students data
-        public string GetStudents = "Student/GetStudents";
+        public static string GetStudents = "Student/GetStudents";
     }
 }

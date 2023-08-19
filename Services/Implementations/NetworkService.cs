5628f28855f565eb6ea86d7eb6ab9a1f86b1b498
Virendra <virendra.thakur31@gmail.com>
2023-08-19 21:28:01 +0530

    NetworkService added.

---
+++ Services/Implementations/NetworkService.cs
@@ -0,0 +1,17 @@
+﻿using MAUISampleApp.MVVM.Services.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MAUISampleApp.MVVM.Services.Implementations
+{
+    public class NetworkService : INetworkService
+    {
+        public bool IsNetworkAvailable()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}

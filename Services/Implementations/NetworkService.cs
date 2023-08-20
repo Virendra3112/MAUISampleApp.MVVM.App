1a54874f1b4f9f49b33d72f1777e4b9262982168
Virendra <virendra.thakur31@gmail.com>
2023-08-20 22:32:37 +0530

    commit.

--- Services/Implementations/NetworkService.cs
+++ Services/Implementations/NetworkService.cs
@@ -8,11 +8,24 @@ namespace MAUISampleApp.MVVM.Services.Implementations
         public NetworkService()
         {
             accessType = Connectivity.Current.NetworkAccess;
+
+            Connectivity.ConnectivityChanged += Connectivity_ConnectivityChanged;
+
         }
+
+        private void Connectivity_ConnectivityChanged(object sender, ConnectivityChangedEventArgs e)
+        {
+            if (e.NetworkAccess == NetworkAccess.ConstrainedInternet)
+                Console.WriteLine("Internet access is available but is limited.");
+
+            else if (e.NetworkAccess != NetworkAccess.Internet)
+                Console.WriteLine("Internet access has been lost.");
+        }
+
         public bool IsNetworkAvailable()
         {
             bool result = false;
-            if (accessType == NetworkAccess.Internet)
+            if (accessType == NetworkAccess.Internet) 
             {
                 // Connection to internet is available
                 result = true;

14c481d784e2aee220f34909bdeaf3f691a44d0e
Virendra <virendra.thakur31@gmail.com>
2023-08-20 22:32:19 +0530

    NetworkStatus added.

---
+++ Models/Enums/NetworkStatus.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MAUISampleApp.MVVM.Models.Enums
+{
+    public enum NetworkStatus
+    {
+        Available,
+        NotAvailable,
+        SlowNetwork,
+    }
+}

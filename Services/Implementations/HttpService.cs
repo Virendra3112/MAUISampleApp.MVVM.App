8916516f12b6c8c521d9fb177c63f3340e005004
Virendra <virendra.thakur31@gmail.com>
2023-08-22 21:30:54 +0530

    HttpService updated.

--- Services/Implementations/HttpService.cs
+++ Services/Implementations/HttpService.cs
@@ -16,29 +16,62 @@ namespace MAUISampleApp.MVVM.Services.Implementations
             {
                 var response = await _httpClient.GetAsync(url);
 
-                if (response == null)
+                if (response == null && response.IsSuccessStatusCode)
                 {
 
                     if (response.StatusCode == HttpStatusCode.OK)
                     {
                         return response;
                     }
+                    else if (response.StatusCode == HttpStatusCode.BadRequest)
+                    {
+                        //check request body
+                    }
                     else
                     {
-                        return null;// HttpStatusCode.BadGateway;
+                        return null;
                     }
                 }
                 return null;
             }
             catch (Exception ex)
             {
-                return null;
+                return null;//ToDo
             }
         }
 
-        public Task<HttpResponseMessage> PostAsync(string url, object data)
+        public async Task<HttpResponseMessage> PostAsync(string url, HttpContent data)
         {
-            throw new NotImplementedException();
+            try
+            {
+                var response = await _httpClient.PostAsync(url, data);
+
+                if (response == null && response.IsSuccessStatusCode)
+                {
+
+                    if (response.StatusCode == HttpStatusCode.OK)
+                    {
+                        return response;
+                    }
+                    else if (response.StatusCode == HttpStatusCode.Unauthorized)
+                    {
+                        //get token and make a request again
+                    }
+                    else if (response.StatusCode == HttpStatusCode.BadGateway)
+                    {
+                        //check request body
+                    }
+                    else
+                    {
+                        return null;
+                    }
+                }
+                return null;
+            }
+            catch (Exception ex)
+            {
+                return null;//ToDo
+            }
         }
     }
 }

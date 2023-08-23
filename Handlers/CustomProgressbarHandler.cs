61d77b49d8116ebf52008098a9512e3ae6f58465
Virendra <virendra.thakur31@gmail.com>
2023-08-23 21:04:28 +0530

    CustomProgressbarHandler added.

---
+++ Handlers/CustomProgressbarHandler.cs
@@ -0,0 +1,23 @@
+﻿using MAUISampleApp.MVVM.CustomControls;
+using SkiaSharp.Views.Maui.Handlers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MAUISampleApp.MVVM.Handlers
+{
+    public static class CustomProgressbarHandler
+    {
+        public static MauiAppBuilder UseProgressBar(this MauiAppBuilder builder)
+        {
+            builder.ConfigureMauiHandlers(h =>
+            {
+                h.AddHandler<CustomProgressBar, SKCanvasViewHandler>();
+            });
+
+            return builder;
+        }
+    }
+}

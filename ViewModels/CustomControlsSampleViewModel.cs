9fba0263dbaa9abd70572e73c9f682a83d90b836
Virendra <virendra.thakur31@gmail.com>
2023-08-28 21:30:32 +0530

    CustomControlsSampleViewModel updated.

--- ViewModels/CustomControlsSampleViewModel.cs
+++ ViewModels/CustomControlsSampleViewModel.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
 
 namespace MAUISampleApp.MVVM.ViewModels
 {
@@ -14,9 +15,23 @@ namespace MAUISampleApp.MVVM.ViewModels
             get { return _progress; }
             set { _progress = value; OnPropertyChanged(); }
         }
+
+        public ICommand ProgressCommand { private set; get; }
         public CustomControlsSampleViewModel()
         {
+            ProgressCommand = new Command(
+            execute: async () =>
+            {
+                await UpdateProgress();
+            });
 
+            ProgressCommand.Execute(null);
+        }
+
+        private async Task UpdateProgress()
+        {
+            //ToDo
+            throw new NotImplementedException();
         }
     }
 }
